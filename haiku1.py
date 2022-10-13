18239170b8ec2ca1c169ca28c34799dc5d8bf7a1
Kyryl <kieremieiev@outlook.com>
2022-10-13 14:45:29 +0200

    the 3rd one is relateble

--- haiku1.py
+++ haiku1.py
@@ -1,12 +1,25 @@
 ## Haiku function template
 def Haiku1():
     print("haiku by:","Kobayahsi Issa")
-    print("From time to time")
-    print("The clouds give rest")
-    print("To the moon-beholders.")
+    print("The wren")
+    print("Earns his living")
+    print("Noiselessly.")
 
-def HaikuX():
+def Haiku2():
     print("haiku by:"," Matsuo Bashō")
     print("From time to time")
     print("The clouds give rest")
     print("To the moon-beholders.")
+
+def Haiku3():
+    print("haiku by:","Yosa Buson, the one who had diarrhea")
+    print("Over-ripe sushi,")
+    print("The Master")
+    print("Is full of regret.")
+
+
+Haiku1()
+print(" ")
+Haiku2()
+print(" ")
+Haiku3()

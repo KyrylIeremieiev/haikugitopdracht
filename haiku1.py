e990864033bb4c9ae1b3f0a3ee9fe5361f44bd48
Kyryl <kieremieiev@outlook.com>
2022-10-13 14:26:18 +0200

    i didnt ask for this, why are you doing this to me

---
+++ haiku1.py
@@ -0,0 +1,6 @@
+## Haiku function template
+def Haiku1():
+    print("haiku by:","Kobayahsi Issa")
+    print("From time to time")
+    print("The clouds give rest")
+    print("To the moon-beholders.")
